6d8c7b583df1717e154de0f3a57125070b4197b2
David López Rguez <dloprodu@gmail.com>
2018-02-15 23:26:33 +0000

    navigation implemented

--- Westeros/Controllers/HouseListViewController.swift
+++ Westeros/Controllers/HouseListViewController.swift
@@ -63,6 +63,12 @@ class HouseListViewController: UITableViewController {
         return cell!
     }
 
+    override func tableView(_ tableview: UITableView, didSelectRowAt: IndexPath) {
+        let house = self.model[didSelectRowAt.row]
+        let houseVC = HouseDetailViewController(model: house)
+        self.navigationController?.pushViewController(houseVC, animated: true)
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
